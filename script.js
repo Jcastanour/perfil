417090f7eb9f28453462e7e9a09572b1f9ea8fda
Juan Pablo C <jcastanour@unal.edu.co>
2024-06-12 22:31:51 -0500

    Logo y reestructuracion 1.0

--- script.js
+++ script.js
@@ -1,43 +1,110 @@
 window.onload = function() {
     actualizarContenidoCopiado();
+    actualizarBotones();
     setInterval(actualizarContenidoCopiado, 0);
+    setInterval(actualizarBotones, 0);
 };
 
+function actualizarBotones(){
+    // let contenidoG = contenido;
+    // let filasG = filas;
+    navigator.clipboard.readText().then(text => {
+    
+        let filasG = 0;
+        let contenidoG = 0;
+        let menuatras = 0;
+
+        
+        document.querySelectorAll('.botones button').forEach(button => {
+            button.style.display = 'none';
+        });
+        document.querySelector('.contenedor-botones').style.display = 'none';
+
+        if (text.includes('\t')) { // Si el texto contiene tabuladores, mostrar como lista
+            filasG = text.trim().split('\n').length;
+            contenidoG = text.trim().split('\t').length;
+        } else if (text.includes('*')){
+            contenidoG = '*'
+        }
+
+        console.log(contenidoG,filasG);
+        
+        if (contenidoG === 2){
+            document.getElementById('plantilladenuevo').style.display = 'inline';
+            document.getElementById('abrirpagina').style.display = 'inline';     
+        } else if (contenidoG === 3) {
+            document.querySelector('.contenedor-botones').style.display = 'inline';
+        } else if ((contenidoG + (filasG-1))% 4 === 0 && (contenidoG + (filasG-1))/4 === (filasG)){
+            document.getElementById('nombresyprecios').style.display = 'inline';       
+        } else if (contenidoG === 5){
+            document.getElementById('generarconcinco').style.display = 'inline';     
+        }else if ((contenidoG + (filasG-1))% 6 === 0 && (contenidoG + (filasG-1))/6 === (filasG)) { // Si el número de celdas es múltiplo de 6
+                document.getElementById('recordardatos').style.display = 'inline';  
+                document.getElementById('disneystarunitario').style.display = 'inline';  
+                document.getElementById('cambiocorreo').style.display = 'inline';  
+                document.getElementById('cambiocontra').style.display = 'inline';  
+                document.getElementById('cambiodisney').style.display = 'inline';  
+                } else if (contenidoG === 7){
+            document.getElementById('plantilladenuevo').style.display = 'inline';  
+            document.getElementById('abrirpagina').style.display = 'inline';  
+            document.getElementById('copiarcuenta').style.display = 'inline';  
+            document.getElementById('contacto').style.display = 'inline';  
+
+        } else if (contenidoG === '*') { // Si el texto contiene asteriscos, aplicar formato especial
+            document.getElementById('copiarexcel').style.display = 'inline';  
+            document.getElementById('copiarcuenta').style.display = 'inline';  
+            document.getElementById('abrirpagina').style.display = 'inline';  
+            document.getElementById('contacto').style.display = 'inline';  
+        }
+    }).catch(error => {
+    });
+}
+
 function actualizarContenidoCopiado() {
+    
     navigator.clipboard.readText()
         .then(text => {
             let contenidoVisualizado = '';
-            if (text.includes('\t')) { // Si el texto contiene tabuladores, mostrar como lista
+
+            if (!text){
+                contenidoVisualizado = `<h2>Debes tener celdas copiadas</h2>`;
+            }
+            else if (text.includes('\t')) { // Si el texto contiene tabuladores, mostrar como lista
                 const filas = text.trim().split('\n');
                 const contenido = text.trim().split('\t');
+
                 if (contenido.length <= 3) { // Si hay 3 celdas, mostrarlas como lista
-                    contenidoVisualizado = '<p>Contenido copiado:</p>';
+                    contenidoVisualizado = `<p>${contenido.length} celda(s) copiada(s).</p>`;
                     contenidoVisualizado += '<div class="lista">';
                     contenido.forEach(item => {
                         contenidoVisualizado += `<div>${item}</div>`;
                     });
                     contenidoVisualizado += '</div>';
-                } else if ((contenido.length > 3 & contenido.length < 6)){
+                } else if ((contenido.length > 1 & contenido.length < 6)){
                     contenidoVisualizado = `<p>${contenido.length} celda(s) copiada(s).</p>`;    
                 }else if (contenido.length >= 6) { // Si hay 6 o más celdas
                     if ((contenido.length + (filas.length-1))% 6 === 0) { // Si el número de celdas es múltiplo de 6
-                        contenidoVisualizado = `<p>${filas.length} fila(s) copiada(s).</p>`;
+                        contenidoVisualizado = `<p>${filas.length} fila(s) copiada(s) de 6 celdas.</p>`;
                     } else if (contenido.length === 7){
                         contenidoVisualizado = `<p>(${contenido.length}) Celdas para pegar en excel</p>`
                     }else { // Si el número de celdas es mayor a 7 celdas
                         contenidoVisualizado = `<p>Atención: El número de celdas copiadas (${contenido.length}) no funciona en ningun caso.</p>`;
                     }}
+                    // actualizarBotones(contenido.length,filas.length);
             } else if (text.includes('*')) { // Si el texto contiene asteriscos, aplicar formato especial
                 const contenido = text.trim().split('\n');
                 contenido.forEach(linea => {
                     contenidoVisualizado += linea.replace(/\*(.*?)\*/g, '<b>$1</b>') + '<br>';
                 });
+                // actualizarBotones('*',0);
             } else { // Si no se cumple ninguna condición, mostrar el texto sin formato especial
                 contenidoVisualizado = text;
             }
+            
             document.getElementById('contenido-copiado').innerHTML = contenidoVisualizado;
-        })
-        .catch(err => console.error('Error al leer el portapapeles:', err));
+
+        }).catch(error => {
+        });
 }
 
 function obtenerFechaFormateada() {
